3cc11d498a74027b9e6810eed77f25fc2922ad1c
Tacoman99 <jorgepadres789@gmail.com>
2024-07-06 13:29:47 -0700

    training pipeline is done finishing up inference pipeline

--- services/price_predictor/src/training.py
+++ services/price_predictor/src/training.py
@@ -1,9 +1,19 @@
+import os
 import pandas as pd
 from loguru import logger
 from typing import Tuple, Optional
 
+from comet_ml import Experiment
+import matplotlib.pyplot as plt
+
 from src.baseline_model import BaselineModel
 from src.feature_engineering import add_features
+from src.data_preprocessing import (
+    interpolate_missing_candles,
+    create_target_metric
+)
+from src.utils import get_model_name
+
 from tools.ohlc_data_reader import OhlcDataReader
 
 
@@ -38,6 +48,24 @@ def train(
         Nothing.
         The model artifact is pushed to the model registry.
     """
+    # Create an experiment to log metadata to CometML
+    experiment = Experiment(
+        api_key=os.environ['COMET_ML_API_KEY'],
+        project_name=os.environ['COMET_ML_PROJECT_NAME'],
+        workspace=os.environ['COMET_ML_WORKSPACE'],
+    )
+
+    # log all the input parameters to the training function
+    experiment.log_parameters({
+        'feature_view_name': feature_view_name,
+        'feature_view_version': feature_view_version,
+        'ohlc_window_sec': ohlc_window_sec,
+        'product_id': product_id,
+        'last_n_days_to_fetch_from_store': last_n_days_to_fetch_from_store,
+        'last_n_days_to_test_model': last_n_days_to_test_model,
+        'prediction_window_sec': prediction_window_sec,
+    })
+
     # Step 1    
     # Fetch the data from the feature store
     ohlc_data_reader = OhlcDataReader(
@@ -50,12 +78,13 @@ def train(
         product_id=product_id,
         last_n_days=last_n_days_to_fetch_from_store,
     )
-    
-    #breakpoint()
 
     # add a column to ohlc_data with a human-readable data, using
     # the ohlc_data['timestamp'] column in milliseconds
     ohlc_data['datetime'] = pd.to_datetime(ohlc_data['timestamp'], unit='ms')
+
+    # log a dataset hash to track the data
+    experiment.log_dataset_hash(ohlc_data)
 
     # Step 2
     # Split the data into training and testing using a cutoff date
@@ -64,9 +93,13 @@ def train(
         ohlc_data=ohlc_data,
         last_n_days_to_test_model=last_n_days_to_test_model,
     )
-    
-    breakpoint()
-    
+
+    # log the number of rows in the training and testing data
+    n_rows_train_original = ohlc_train.shape[0]
+    n_rows_test_original = ohlc_test.shape[0]
+    experiment.log_metric('n_rows_train', n_rows_train_original)
+    experiment.log_metric('n_rows_test', n_rows_test_original)
+
     # Step 3
     # Preprocess the data for training and for testing
     # Interpolate missing candles
@@ -75,50 +108,64 @@ def train(
     logger.info('Interpolating missing candles for testing data')
     ohlc_test = interpolate_missing_candles(ohlc_test, ohlc_window_sec)
 
-    breakpoint()
-    
+    # let's log the number rows that had to be interpolated because missing data
+    n_interpolated_rows_train = ohlc_train.shape[0] - n_rows_train_original
+    n_interpolated_rows_test = ohlc_test.shape[0] - n_rows_test_original
+    experiment.log_metric('n_interpolated_rows_train', n_interpolated_rows_train)
+    experiment.log_metric('n_interpolated_rows_test', n_interpolated_rows_test)
+
     # Step 4
     # Create the target metric as a new column in our dataframe for training and testing
     logger.info('Creating the target metric')
     ohlc_train = create_target_metric(
         ohlc_train,
         ohlc_window_sec,
-        # discretization_thresholds,
         prediction_window_sec,
     )
     ohlc_test = create_target_metric(
         ohlc_test,
         ohlc_window_sec,
-        # discretization_thresholds,
         prediction_window_sec,
     )
-    
-    breakpoint()
+
+    # create a histogram of the continuous variable ohlc_train['target']
+    # using matplotlib and save it to an object
+    # TODO: check why this plot is not logged to CometML
+    plt.figure(figsize=(10, 6))
+    plt.hist(ohlc_train['target'], bins=30, alpha=0.75, color='blue', edgecolor='black')
+    plt.title('Histogram of Price Change')
+    plt.xlabel('Price change')
+    plt.ylabel('Frequency')
+    plt.grid(True)
+    # push this object as a figure to CometML
+    experiment.log_figure(figure=plt)
 
     # Before training, let's split the features and the target
     X_train = ohlc_train.drop(columns=['target'])
     y_train = ohlc_train['target']
     X_test = ohlc_test.drop(columns=['target'])
     y_test = ohlc_test['target']
-    
+
     # Step 5
     # Let's build a baseline model
     model = BaselineModel(
         n_candles_into_future=prediction_window_sec // ohlc_window_sec,
-        # discretization_thresholds=discretization_thresholds,
     )
     y_test_predictions = model.predict(X_test)
-    evaluate_model(
+    baseline_test_mae = evaluate_model(
         predictions=y_test_predictions,
         actuals=y_test,
         description='Baseline model on Test data',
     )
     y_train_predictions = model.predict(X_train)
-    evaluate_model(
+    baseline_train_mae = evaluate_model(
         predictions=y_train_predictions,
         actuals=y_train,
         description='Baseline model on Training data',
     )
+    # log the mean absolute error of the baseline model, both on training and testing data
+    experiment.log_metric('baseline_model_mae_test', baseline_test_mae)
+    experiment.log_metric('baseline_model_mae_train', baseline_train_mae)
 
     # Step 6
     # Build a more complex model
@@ -130,6 +177,7 @@ def train(
         X_test,
         n_candles_into_future=prediction_window_sec // ohlc_window_sec,
     )
+    # Idea: You could extract this list as a config parameter in your src.config module
     features_to_use = [
         'rsi',
         'momentum',
@@ -146,6 +194,15 @@ def train(
     X_train = X_train[features_to_use]
     X_test = X_test[features_to_use]
 
+    # log the shapes of X_train, y_train, X_test, y_test
+    experiment.log_metric('X_train_shape', X_train.shape)
+    experiment.log_metric('y_train_shape', y_train.shape)
+    experiment.log_metric('X_test_shape', X_test.shape)
+    experiment.log_metric('y_test_shape', y_test.shape)
+
+    # log the list of feature names
+    experiment.log_parameter('features_to_use', features_to_use)
+
     # train a lasso regression model
     from src.model_factory import fit_lasso_regressor 
     model = fit_lasso_regressor(
@@ -153,34 +210,72 @@ def train(
         y_train,
         tune_hyper_params=False,
     )
-    evaluate_model(
+    test_mae = evaluate_model(
         predictions=model.predict(X_test),
         actuals=y_test,
         description='Lasso regression model on Test data',
     )
-    evaluate_model(
+    train_mae = evaluate_model(
         predictions=model.predict(X_train),
         actuals=y_train,
         description='Lasso regression model on Training data',
     )
 
-    # train an XGBoost model
-    from src.model_factory import fit_xgboost_regressor
-    model = fit_xgboost_regressor(
-        X_train,
-        y_train,
-        tune_hyper_params=False,
-    )
-    evaluate_model(
-        predictions=model.predict(X_test),
-        actuals=y_test,
-        description='XGBoost regression model on Test data',
-    )
-    evaluate_model(
-        predictions=model.predict(X_train),
-        actuals=y_train,
-        description='XGBoost regression model on Training data',
-    )
+    # log the mean absolute error of the lasso regression model, both on training and testing data
+    experiment.log_metric('lasso_model_mae_test', test_mae)
+    experiment.log_metric('lasso_model_mae_train', train_mae)
+
+    # TODO: for the next cohort of this course, in September 2024.
+    # XGBoost is overfitting the data. There are no strong patterns (aka correlations)
+    # between the features and the target. So XGBoost ends up fitting noise, not signal.
+    # Which means that the error metric on the training is good (no surprise, it's fitting noise)
+    # but the error metric on the testing data is bad (it's not generalizing well)
+    # Because of this, I decided to comment out this section.
+    # If you enlarge the feature representation building 50 more technical indicators,
+    # it might be worth trying XGBoost again.
+    #
+    # # train an XGBoost model
+    # from src.model_factory import fit_xgboost_regressor
+    # model = fit_xgboost_regressor(
+    #     X_train,
+    #     y_train,
+    #     tune_hyper_params=False,
+    # )
+    # evaluate_model(
+    #     predictions=model.predict(X_test),
+    #     actuals=y_test,
+    #     description='XGBoost regression model on Test data',
+    # )
+    # evaluate_model(
+    #     predictions=model.predict(X_train),
+    #     actuals=y_train,
+    #     description='XGBoost regression model on Training data',
+    # )
+
+    # Step X
+    # Save the model as pickle file
+    import pickle
+    with open('./lasso_model.pkl', 'wb') as f:
+        logger.debug('Saving the model as a pickle file')
+        pickle.dump(model, f)
+    
+    model_name = get_model_name(product_id)
+    experiment.log_model(name=model_name, file_or_folder='./lasso_model.pkl')
+
+    # Last step in your training pipeline, is to push the model to the model registry
+    # if you are happy with the performance of the model
+    
+    # In this case I want to push the model to the model registry, no matter its performance
+    # because I want us to move on to the next step in the project, which is the
+    # inference pipeline and the deployment.
+    # if test_mae < baseline_test_mae:
+    if True:
+
+        # push the model to the model registry
+        experiment.register_model(
+            model_name=model_name,
+        )
+        # breakpoint()
 
     
 
@@ -188,7 +283,7 @@ def evaluate_model(
     predictions: pd.Series,
     actuals: pd.Series,
     description: Optional[str] = 'Model evaluation',
-):
+) -> float:
     """
     Evaluates the model using accuracy, confusion matrix and classification report.
 
@@ -198,7 +293,7 @@ def evaluate_model(
         description (str): A description of the model and the data.
 
     Returns:
-        Nothing.
+        float: The mean absolute error.
     """
     logger.info('****' + description + '****')
 
@@ -208,6 +303,8 @@ def evaluate_model(
     mae = mean_absolute_error(actuals, predictions)
     # log the mean absolute error with exponential notation
     logger.info('Mean Absolute Error: %.4e' % mae)
+
+    return mae
 
 
 def split_train_test(
@@ -234,99 +331,18 @@ def split_train_test(
     return ohlc_train, ohlc_test
 
 
-def create_target_metric(
-    ohlc_data: pd.DataFrame,
-    ohlc_window_sec: int,
-    # discretization_thresholds: list,
-    prediction_window_sec: int,
-) -> pd.DataFrame:
-    """
-    Creates the target metric by 
-    - discretizing the close price in the next prediction_window_sec
-    - using the discretization_thresholds
-    - and adds the final column in the output dataframe
-
-    Args:
-        ohlc_data (pd.DataFrame): The OHLC data.
-        ohlc_window_sec (int): The size of the window in seconds.
-        discretization_thresholds (list): The thresholds to discretize the close price.
-        prediction_window_sec (int): The size of the prediction window in seconds.
-
-    Returns:
-        pd.DataFrame: The OHLC data with the target metric.
-    """
-    # check that prediction_window_sec is a multiple of ohlc_window_sec
-    assert prediction_window_sec % ohlc_window_sec == 0, \
-        'prediction_window_sec must be a multiple of ohlc_window_sec'
-
-    n_candles_into_future = prediction_window_sec // ohlc_window_sec
-
-    # create a new column with the percentage change in the close price n_candles_into_future
-    ohlc_data['close_pct_change'] = ohlc_data['close'].pct_change(n_candles_into_future)
-    
-    # shift the target column by n_candles_into_future to have the target for the current candle
-    ohlc_data['target'] = ohlc_data['close_pct_change'].shift(-n_candles_into_future)
-
-    # drop the close_pct_change column
-    ohlc_data.drop(columns=['close_pct_change'], inplace=True)
-
-    # Anton Javelosa asked:
-    # "Won't n rows at the end have blank targets? After the shift up?"
-    # Yes, you are right. Let's drop them before returning the dataframe
-    # Thanks Anton!
-    ohlc_data.dropna(subset=['target'], inplace=True)
-
-    return ohlc_data
-
-
-def interpolate_missing_candles(
-    ohlc_data: pd.DataFrame,
-    ohlc_window_sec: int,
-) -> pd.DataFrame:
-    """
-    Interpolates missing candles in the OHLC data.
-
-    Args:
-        ohlc_data (pd.DataFrame): The OHLC data.
-        ohlc_window_sec (int): The size of the window in seconds.
-
-    Returns:
-        pd.DataFrame: The OHLC data with the missing candles interpolated.
-    """
-    # Python trick!
-    # We use inplace to avoid copying the data, which should be more efficient
-    ohlc_data.set_index('timestamp', inplace=True)
-    # otherwise you can do a copy. This is less efficient.
-    # ohlc_data = ohlc_data.set_index('timestamp')
-    
-    # complete list of timestamps for which we need to have rows in our dataframe
-    from_ms = ohlc_data.index.min()
-    to_ms = ohlc_data.index.max()
-    labels = range(from_ms, to_ms, ohlc_window_sec*1000)
-    # reindex the dataframe to add missing rows
-    ohlc_data = ohlc_data.reindex(labels)
-
-    # interpolate missing values using forward fill for close prices
-    ohlc_data['close'].ffill(inplace=True)
-
-    # if ohlc_data['open] is null use the ohlc_data['close'] value
-    ohlc_data['open'].fillna(ohlc_data['close'], inplace=True)
-    # do the same for high and low
-    ohlc_data['high'].fillna(ohlc_data['close'], inplace=True)
-    ohlc_data['low'].fillna(ohlc_data['close'], inplace=True)
-    
-    # we have to forward fill the product_id as well
-    ohlc_data['product_id'].ffill(inplace=True)
-
-    # reset the index
-    ohlc_data.reset_index(inplace=True)
-
-    # let's make sure we have no missing datetimes
-    ohlc_data['datetime'] = pd.to_datetime(ohlc_data['timestamp'], unit='ms')
-    
-    return ohlc_data
 
 if __name__ == '__main__':
+
+    train(
+        feature_view_name='ohlc_feature_view',
+        feature_view_version=1,
+        ohlc_window_sec=60,
+        product_id='BTC/USD',
+        last_n_days_to_fetch_from_store=90,
+        last_n_days_to_test_model=7,
+        prediction_window_sec=60*5,
+    )
 
     train(
         feature_view_name='ohlc_feature_view',
